36a69e03a3dfa4c1372949b354178e6626105cde
shubham_wadne <kaylekashubham@gmail.com>
2024-01-19 19:01:09 +0530

    second commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,8 +6,9 @@ const About = () => {
             <img className="w-full" src="/aboutbg.png" alt="" />
             <h1 className="absolute -translate-x-1/2 left-1/2 top-1/3 text-5xl font-extrabold">About</h1>
             <div className="absolute -translate-x-1/2 left-1/2 top-96">
-                <h3 className="font-extrabold text-center text-lg">Yo, this is Doug the Pug, the degen doggo behind $DOUG coin. Me and my homies Reggie and Demarcus, we're just some strays tryna make it off the streets. We lost it all on some shitcoins, but now we got a new plan - $DOUG. Our mission? To get this meme coin to the moon so we can pay off our debts, get some grub, and party with the bitches. We ain't got no exchange listings yet, but we're counting on the community to help us out. Join the pack, dont jeet and let's make $DOUG the next big thing in crypto!</h3>
+                <h3 className="font-extrabold text-center text-lg">Yo, this is Doug the Pug, the degen doggo behind $DOUG coin. Me and my homies Reggie and Demarcus, we&apos;re just some strays tryna make it off the streets. We lost it all on some shitcoins, but now we got a new plan - $DOUG. Our mission? To get this meme coin to the moon so we can pay off our debts, get some grub, and party with the bitches. We ain&apos;t got no exchange listings yet, but we&apos;re counting on the community to help us out. Join the pack, dont jeet and let&apos;s make $DOUG the next big thing in crypto!</h3>
             </div>
+            <img src="" alt="" />
         </div>
     )
 }
